69b7660ee672a53d451b8750dcae5c1aee09468b
liuzheng <83042609@qq.com>
2017-04-17 18:01:45 +0800

    1

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -5,14 +5,15 @@ from ..models import Post,Category
 
 register = template.Library()
 
+
 @register.simple_tag
 def get_recent_posts(num=5):
     return Post.objects.all()[:num]
 
 
 @register.simple_tag
-def archivessels():
-    return Post.objects.dates('created_time','month',order='DESC')
+def archives():
+    return Post.objects.dates('created_time', 'month', order='DESC')
 
 
 @register.simple_tag
